3ca319bdcfd1983f0268d4a7a653b6fcfa288cbe
dheeraj755148 <dheerajdayakaran95@gmail.com>
2021-02-11 19:24:20 +0530

    nav, loading speed fixed

--- loader.js
+++ loader.js
@@ -1,15 +1,12 @@
-document.onreadystatechange = function() { 
-    if (document.readyState !== "complete") { 
-        document.querySelector( 
-          "body").style.visibility = "hidden"; 
-        document.querySelector( 
-          "#loader").style.visibility = "visible"; 
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
 
-   
-    } else { 
-        document.querySelector( 
-          "#loader").style.display = "none"; 
-        document.querySelector( 
-          "body").style.visibility = "visible"; 
-    } 
-}; 
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+    var deletion = document.getElementById("preload")
+    deletion.classList.remove("preloader-stuff")
+  }
+};
